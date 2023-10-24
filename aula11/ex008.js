81ecf6cf63229dcf0a54d12700f042008811ef5f
LucasVilasBooas <146893322+LucasVilasBooas@users.noreply.github.com>
2023-10-24 13:29:16 -0300

    desafio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel>60) {
+    console.log(`voce ultrapassou a velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
