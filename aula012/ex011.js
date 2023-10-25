111bfdcaa7d193c7d6486475c87e4957fd7da880
LucasVilasBooas <146893322+LucasVilasBooas@users.noreply.github.com>
2023-10-24 22:20:46 -0300

    up

---
+++ aula012/ex011.js
@@ -0,0 +1,9 @@
+var idade = 77
+if (idade < 16) {
+    console.log('Nao vota')
+} else if (idade < 18 || idade > 65){
+    console.log('voto opicional')
+}else {
+    console.log('voto obrigatorio')
+}
+
