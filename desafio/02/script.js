b46658020a7aabfbb6f1b4f8dc6a5ba78d34ab66
LucasVilasBooas <146893322+LucasVilasBooas@users.noreply.github.com>
2023-10-27 02:51:04 -0300

    pessoas ok

--- desafio/02/script.js
+++ desafio/02/script.js
@@ -3,13 +3,41 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('ERRO! Verifique o ano e tente novamente.')
-    } else if  (xxxx) {
-        img.src = 'tarde.jpg'
-        
     } else {
-        img.src = 'noite.jpg'
-        
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero =''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'masculino'
+            if (idade >= 0 && idade < 10){
+                // criança
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade <60 ){
+                // adiçtp
+                img.setAttribute('src', 'homem.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso.jpeg')
+            }
+        } else if (fsex[1].checked) {
+            genero = "feminino"
+            if (idade >= 0 && idade < 10){
+                // criança
+                img.setAttribute('src', 'menina.jpg')
+            } else if (idade <60 ){
+                // adiçtp
+                img.setAttribute('src', 'mulher.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosa.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos o genero ${genero} com ${idade} anos`
+        res.appendChild(img)   
     }
 }
