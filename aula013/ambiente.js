ed076c5f8a388eafde383ce522a7917172b949e5
LucasVilasBooas <146893322+LucasVilasBooas@users.noreply.github.com>
2023-10-27 18:19:30 -0300

    repetições

---
+++ aula013/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+console.log('acima foi o WHILE .... abaixo é o DO')
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 3)
