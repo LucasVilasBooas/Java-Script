dd7d03610097980a7f811a1ec598b43237969879
LucasVilasBooas <146893322+LucasVilasBooas@users.noreply.github.com>
2023-11-02 22:20:13 -0300

    up

--- desafio/05/script.js
+++ desafio/05/script.js
@@ -2,7 +2,6 @@ let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
-
 function isNumero(n) {
     if (Number(n) >= 1 && Number(n) <= 100){
         return true
@@ -10,28 +9,26 @@ function isNumero(n) {
         return false
     }
 }
-
 function inLista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
+    if (l.indexOf(Number(n)) != -1) { 
         return true
     } else {
         return false
     }
 }
-
 function adicionar(){
-    if (isNumero(num.value) && !inLista(num.value)) {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor invalido ou ja encontrado na lista')
     }
     num.value =''
     num.focus()
 }
-
 function finalizar() {
     if (valores.length == 0) {
         window.alert('Adicione valores antes de FINALIZAR')
